c532e82d58571480bb1004c6f74d93cc2d565c66
rodr1to <rovalverdea@gmail.com>
2025-10-07 15:46:42 -0500

    update: added cart logic to add/remove item products

--- EasyShop/Presentation/Cart/CartView.swift
+++ EasyShop/Presentation/Cart/CartView.swift
@@ -20,19 +20,13 @@ struct CartView: View {
                         .swipeActions(edge: .trailing) {
                                         
                                         Button(role: .destructive){
-                                            /*
-                                            if let index = cartItems.firstIndex(where: { $0.product.name == item.product.name }) {
-                                                cartItems.remove(at: index)
-                                            }
-                                            */
+                                            viewModel.removeCartItem(product: item.product)
                             } label: {
                                 Label("Remove", systemImage: "trash")
                             }
                         }
                 }
-                .onDelete{ _ in
-                }
-                
+
                 .listRowSeparator(.hidden)
             }
             .listStyle(.plain)
